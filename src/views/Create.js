7a49a7c931cdda1b1b6681aa36bb7567e16fb2e3
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-10-23 11:48:40 -0500

    solution

--- src/views/Create.js
+++ src/views/Create.js
@@ -1,65 +1,11 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { createCard } from '../api/data/cardData';
-
-const initialState = {
-  title: '',
-  description: '',
-};
+import React from 'react';
+import CardForm from '../components/CardForm';
 
 export default function Create() {
-  const [formInput, setFormInput] = useState(initialState);
-  const history = useHistory();
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setFormInput((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const resetForm = () => {
-    setFormInput(initialState);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    createCard(formInput).then(() => {
-      resetForm();
-      history.push('/');
-    });
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <h1 className="text-center">Create Card</h1>
-      <div className="m-3">
-        <input
-          type="text"
-          className="form-control"
-          name="title"
-          value={formInput.title}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="m-3">
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-          name="description"
-          value={formInput.description}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="m-3">
-        <button type="submit" className="btn btn-success">
-          Submit
-        </button>
-      </div>
-    </form>
+      <CardForm />
+    </>
   );
 }
